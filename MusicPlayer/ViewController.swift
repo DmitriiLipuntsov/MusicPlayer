6aab75e497cd1221a352a1fdc54356d5d1c023c0
dmitriiLipuntsov <DmitriiLipuntsov@yandex.ru>
2020-06-02 11:46:10 +0500

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Михаил Липунцов on 02/06/2020.
+//  Copyright © 2020 Дмитрий липунцов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
