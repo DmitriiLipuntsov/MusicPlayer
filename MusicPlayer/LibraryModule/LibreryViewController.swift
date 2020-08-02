26081ef97a08838e871c9aec79fc84e76fe80421
dmitriiLipuntsov <DmitriiLipuntsov@yandex.ru>
2020-08-02 21:15:40 +0500

    3) refactoring deletes

---
+++ MusicPlayer/LibraryModule/LibreryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LibreryViewController.swift
+//  MusicPlayer
+//
+//  Created by Михаил Липунцов on 02.08.2020.
+//  Copyright © 2020 Дмитрий липунцов. All rights reserved.
+//
+
+import UIKit
+
+class LibreryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
