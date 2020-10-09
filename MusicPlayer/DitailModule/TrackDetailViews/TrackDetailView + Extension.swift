7b6148c018a9b54e7c866e54f10b2ac9fb467c33
dmitriiLipuntsov <DmitriiLipuntsov@yandex.ru>
2020-10-09 16:11:57 +0500

    small changes in the layout of the DetailView

--- MusicPlayer/DitailModule/TrackDetailViews/TrackDetailView + Extension.swift
+++ MusicPlayer/DitailModule/TrackDetailViews/TrackDetailView + Extension.swift
@@ -16,6 +16,8 @@ extension TrackDetailView {
         button.translatesAutoresizingMaskIntoConstraints = false
         addSubview(button)
         
+        button.setImage(UIImage(named: "DragDown"), for: .normal)
+        
         button.leftAnchor.constraint(equalTo: leftAnchor, constant: 30).isActive = true
         button.rightAnchor.constraint(equalTo: rightAnchor, constant: -30).isActive = true
         button.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
@@ -24,145 +26,74 @@ extension TrackDetailView {
         return button
     }
     
-    func makeTrackImageView() -> UIImageView {
-        let imageView = UIImageView()
+    func makeAllStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.distribution = .equalSpacing
         
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(imageView)
-        imageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
-        imageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -40).isActive = true
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1).isActive = true
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
         
-        return imageView
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: dragDownButton.bottomAnchor, constant: 10),
+            stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
+            ])
+        
+        return stackView
     }
     
-    func makeSliderCurrentTime() -> UISlider {
-        let slider = UISlider()
+    func makeTimeStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .vertical
         
-        slider.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(slider)
-        
-        slider.leftAnchor.constraint(equalTo: leftAnchor, constant: 4).isActive = true
-        slider.rightAnchor.constraint(equalTo: rightAnchor, constant: -4).isActive = true
-        slider.topAnchor.constraint(equalTo: trackImageView.bottomAnchor, constant: 10).isActive = true
-        
-        return slider
+        return stackView
     }
     
-    func makeCurrentTimeLabel() -> UILabel {
-        let label = UILabel()
+    func makeTimeLabelStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
-        label.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(label)
+        addSubview(stackView)
         
-        label.leftAnchor.constraint(equalTo: leftAnchor, constant: 0).isActive = true
-        label.rightAnchor.constraint(equalTo: rightAnchor, constant: -bounds.width / 2).isActive = true
-        label.topAnchor.constraint(equalTo: currentTimeSlider.bottomAnchor, constant: 0).isActive = true
-        label.textAlignment = .left
-        label.textColor = #colorLiteral(red: 0.5637609363, green: 0.5688328743, blue: 0.5901173949, alpha: 1)
-        label.font = UIFont.systemFont(ofSize: 15)
+        NSLayoutConstraint.activate([
+            stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10)
+            ])
         
-        return label
+        return stackView
     }
     
-    func makeDurationLabel() -> UILabel {
-        let label = UILabel()
+    func makeNameLabelStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .vertical
         
-        label.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(label)
-        
-        label.leftAnchor.constraint(equalTo: leftAnchor, constant: bounds.width / 2).isActive = true
-        label.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
-        label.topAnchor.constraint(equalTo: currentTimeSlider.bottomAnchor, constant: 0).isActive = true
-        label.textAlignment = .right
-        label.textColor = #colorLiteral(red: 0.5637609363, green: 0.5688328743, blue: 0.5901173949, alpha: 1)
-        label.font = UIFont.systemFont(ofSize: 15)
-        
-        return label
-    }
-    
-    func makeTrackNameLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(label)
-        
-        label.leftAnchor.constraint(equalTo: leftAnchor, constant: 0).isActive = true
-        label.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
-        label.topAnchor.constraint(equalTo: durationLabel.bottomAnchor, constant: 10).isActive = true
-        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
-        label.textAlignment = .center
-        
-        return label
-    }
-    
-    func makeArtistNameLabel() -> UILabel {
-        let label = UILabel()
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(label)
-        
-        label.leftAnchor.constraint(equalTo: leftAnchor, constant: 0).isActive = true
-        label.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
-        label.topAnchor.constraint(equalTo: trackNameLabel.bottomAnchor, constant: 0).isActive = true
-        label.font = UIFont.systemFont(ofSize: 24, weight: .light)
-        label.textAlignment = .center
-        label.textColor = #colorLiteral(red: 0.9889323115, green: 0.1831878126, blue: 0.3349292278, alpha: 1)
-        
-        return label
+        return stackView
     }
     
     func makeTrackControlView() -> TrackControlView {
         let view = TrackControlView()
-        
         view.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(view)
-        
-        view.leftAnchor.constraint(equalTo: leftAnchor, constant: 0).isActive = true
-        view.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
-        view.topAnchor.constraint(equalTo: artistNameLabel.bottomAnchor, constant: 10).isActive = true
-        view.heightAnchor.constraint(equalToConstant: 105).isActive = true
-        
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalToConstant: 200),
+            view.heightAnchor.constraint(equalToConstant: 100),
+            ])
         return view
     }
     
-    func makeMinVolumeButton() -> UIButton {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(button)
+    func makeVolumeStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.spacing = 10
         
-        button.leftAnchor.constraint(equalTo: leftAnchor, constant: 0).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 17).isActive = true
-        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
-        button.topAnchor.constraint(equalTo: trackControlView.bottomAnchor, constant: 16).isActive = true
-        
-        return button
-    }
-    
-    func makeVolumeSlider() -> UISlider {
-        let slider = UISlider()
-        
-        slider.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(slider)
-        
-        slider.leftAnchor.constraint(equalTo: minVolumeButton.rightAnchor, constant: 10).isActive = true
-        slider.rightAnchor.constraint(equalTo: maxVolumeButton.leftAnchor, constant: -10).isActive = true
-        slider.topAnchor.constraint(equalTo: trackControlView.bottomAnchor, constant: 10).isActive = true
-        
-        return slider
-    }
-    
-    func makeMaxVolumeButton() -> UIButton {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(button)
-        
-        button.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 17).isActive = true
-        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
-        button.topAnchor.constraint(equalTo: minVolumeButton.topAnchor).isActive = true
-        
-        return button
+        return stackView
     }
     
 }
--- MusicPlayer/DitailModule/TrackDetailViews/TrackDetailView.swift
+++ MusicPlayer/DitailModule/TrackDetailViews/TrackDetailView.swift
@@ -11,30 +11,112 @@ import UIKit
 class TrackDetailView: UIView {
     
     lazy var dragDownButton = makeDragDownButton()
-    lazy var trackImageView = makeTrackImageView()
-    lazy var currentTimeSlider = makeSliderCurrentTime()
-    lazy var currentTimeLabel = makeCurrentTimeLabel()
-    lazy var durationLabel = makeDurationLabel()
-    lazy var trackNameLabel = makeTrackNameLabel()
-    lazy var artistNameLabel = makeArtistNameLabel()
-    lazy var trackControlView = makeTrackControlView()
-    lazy var minVolumeButton = makeMinVolumeButton()
-    lazy var maxVolumeButton = makeMaxVolumeButton()
-    lazy var volumeSlider = makeVolumeSlider()
+    lazy var allStackView = makeAllStackView()
+    lazy var timeStackView = makeTimeStackView()
+    lazy var timeLabelStackView = makeTimeLabelStackView()
+    lazy var nameLabelStackView = makeNameLabelStackView()
+    lazy var trackControlView =  makeTrackControlView()
+    lazy var volumeStackView = makeVolumeStackView()
+    
+    var trackImageView = UIImageView()
+    var currentTimeSlider = UISlider()
+    
+    var currentTimeLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textAlignment = .left
+        label.textColor = #colorLiteral(red: 0.5637609363, green: 0.5688328743, blue: 0.5901173949, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.text = "00:00"
+        
+        return label
+    }()
+    
+    var durationLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textAlignment = .right
+        label.textColor = #colorLiteral(red: 0.5637609363, green: 0.5688328743, blue: 0.5901173949, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.text = "--:--"
+        
+        return label
+    }()
+    
+    var trackNameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    var artistNameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont.systemFont(ofSize: 24, weight: .light)
+        label.textAlignment = .center
+        label.textColor = #colorLiteral(red: 0.9889323115, green: 0.1831878126, blue: 0.3349292278, alpha: 1)
+        
+        return label
+    }()
+    
+    var minVolumeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "IconMin"), for: .normal)
+        return button
+    }()
+    
+    var maxVolumeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "IconMax"), for: .normal)
+        return button
+    }()
+    
+    var volumeSlider: UISlider = {
+        let slider = UISlider()
+        slider.value = 1
+        return slider
+    }()
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        dragDownButton.setImage(#imageLiteral(resourceName: "DragDown"), for: .normal)
-        currentTimeSlider.value = 0
-        currentTimeLabel.text = "00:00"
-        durationLabel.text = "--:--"
-        minVolumeButton.setImage(#imageLiteral(resourceName: "IconMin"), for: .normal)
-        maxVolumeButton.setImage(#imageLiteral(resourceName: "IconMax"), for: .normal)
-        volumeSlider.value = 1
-        if self.bounds.height > 667 {
-        trackImageView.topAnchor.constraint(equalTo: dragDownButton.bottomAnchor, constant: self.bounds.height / 12).isActive = true
-        } else {
-            trackImageView.topAnchor.constraint(equalTo: dragDownButton.bottomAnchor, constant: 10).isActive = true
-        }
+        
+        addStacksView()
+        setConstrintForSliders()
+        
+    }
+    
+    func addStacksView() {
+        allStackView.addArrangedSubview(trackImageView)
+        allStackView.addArrangedSubview(timeStackView)
+        allStackView.addArrangedSubview(nameLabelStackView)
+        allStackView.addArrangedSubview(trackControlView)
+        allStackView.addArrangedSubview(volumeStackView)
+        
+        timeStackView.addArrangedSubview(currentTimeSlider)
+        timeStackView.addArrangedSubview(timeLabelStackView)
+        
+        timeLabelStackView.addArrangedSubview(currentTimeLabel)
+        timeLabelStackView.addArrangedSubview(durationLabel)
+        
+        nameLabelStackView.addArrangedSubview(trackNameLabel)
+        nameLabelStackView.addArrangedSubview(artistNameLabel)
+        
+        volumeStackView.addArrangedSubview(minVolumeButton)
+        volumeStackView.addArrangedSubview(volumeSlider)
+        volumeStackView.addArrangedSubview(maxVolumeButton)
+    }
+    
+    func setConstrintForSliders() {
+        allStackView.heightAnchor.constraint(equalToConstant: bounds.height * 0.85).isActive = true
+        
+        timeStackView.addConstraint(NSLayoutConstraint(item: currentTimeSlider, attribute: .left, relatedBy: .equal, toItem: timeStackView, attribute: .left, multiplier: 1, constant: 0))
+        timeStackView.addConstraint(NSLayoutConstraint(item: currentTimeSlider, attribute: .right, relatedBy: .equal, toItem: timeStackView, attribute: .right, multiplier: 1, constant: 0))
+        
+        volumeStackView.addConstraint(NSLayoutConstraint(item: volumeSlider, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: bounds.width * 0.8))
+        
+        trackImageView.heightAnchor.constraint(equalToConstant: bounds.height * 0.35).isActive = true
     }
 }
